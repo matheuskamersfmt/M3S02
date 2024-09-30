4f472f7a558282f50335cf4cdf9dd89386dd99be
Matheus <matheus.kamers@checkstore.com.br>
2024-09-30 20:11:11 -0300

    Exercício 1

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end('Hello World, fundamentos nodejs aplicados.');
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
